b74f190d281cee0f83d5bb49d19bcd3600de8ad9
francoromeo <francoromeo5012@gmail.com>
2023-03-20 22:51:36 -0300

    downlader image

--- main.js
+++ main.js
@@ -5,9 +5,18 @@ async function requestInfo() {
     let html = "";
     for (let i = 0; i <json.posts.length; i++){
         const post = json.posts[i];
+        const [profileURL, postURL] = await Promise.all([
+
+            downloaderImage(post.profile.photo),
+            downloaderImage(post.content.photo),
+        ])
     }
 }
 async function downloaderImage(url){
     console.log(`Descargando ${url}...`);
     const response = await fetch(`http://localhost:5500/img/${url}`);
+    const blob = await response.blob();
+    const imgURL = URL.createObjectURL(blob);
+    console.log(`Descarga completa de ${url}`);
+    return imgURL;
 }
